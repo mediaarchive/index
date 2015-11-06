51a3247f58859480a86340b218490d20e57f236d
atnartur <atnartur2012@gmail.com>
2015-11-06 21:45:30 +0300

    + base_dir у dirs.start() = некоторая оптимизация

--- dirs.js
+++ dirs.js
@@ -15,51 +15,53 @@ function dir_is_month_or_day(dir_name) {
         return false;
 }
 
-exports.start = function(){
+exports.start = function(base_dir){
+    if(typeof base_dir === 'undefined')
+        base_dir = global.config.archive_dir;
     var result = [];
     
-    var years = fs.readdirSync(global.config.archive_dir);
+    var years = fs.readdirSync(base_dir);
     
     years.forEach(function(year){ // цикл по годам
         if (year == '.' || year == '..')
-            return false;
+            return;
         
         if (!dir_is_year(year)) { // если это не год
             console.log('Error: dir "' + year + '" not a year dir');
-            return false;
+            return;
         }
             
-        var months = fs.readdirSync(global.config.archive_dir + '/' + year); 
+        var months = fs.readdirSync(base_dir + '/' + year); 
         
         months.forEach(function(month){
             if (month == '.' || month == '..')
-                return false;
+                return;
             
             if (!dir_is_month_or_day(month)) { // если это не месяц
                 console.log('Error: dir "' + year + '/' + month + '" not a month dir');
-                return false;
+                return;
             }
             
-            var days = fs.readdirSync(global.config.archive_dir + '/' + year + '/' + month);
+            var days = fs.readdirSync(base_dir + '/' + year + '/' + month);
             
             days.forEach(function(day){
                 if (day == '.' || day == '..')
-                    return false;
+                    return;
                 
                 if (!dir_is_month_or_day(day)) { // если это не месяц
                     console.log('Error: dir "' + year + '/' + month + '/' + day + '" not a day dir');
-                    return false;
+                    return;
                 }
                 
-                var events = fs.readdirSync(global.config.archive_dir + '/' + year + '/' + month + '/' + day);
+                var events = fs.readdirSync(base_dir + '/' + year + '/' + month + '/' + day);
                 
                 events.forEach(function(event){
                     if (event == '.' || event == '..')
-                        return false;
+                        return;
                     
-                    if(!fs.lstatSync(global.config.archive_dir + '/' + year + '/' + month + '/' + day + '/' + event).isDirectory()){
+                    if(!fs.lstatSync(base_dir + '/' + year + '/' + month + '/' + day + '/' + event).isDirectory()){
                         console.log('Error: dir "' + year + '/' + month + '/' + day + '/' + event + '" not event dir');
-                        return false;
+                        return;
                     }
                     
                     var event_res = {
@@ -72,40 +74,28 @@ exports.start = function(){
                         photos: false,
                         preview_photo: '',
                         dir: year + '/' + month + '/' + day + '/' + event
-                    }
+                    };
                     
-                    if(fs.existsSync(global.config.archive_dir + '/' + year + '/' + month + '/' + day + '/' + event + '/фото'))
+                    if(fs.existsSync(base_dir + '/' + year + '/' + month + '/' + day + '/' + event + '/фото'))
                         event_res.photos = true;
                     
-                    var files = fs.readdirSync(global.config.archive_dir + '/' + year + '/' + month + '/' + day + '/' + event);
+                    var files = fs.readdirSync(base_dir + '/' + year + '/' + month + '/' + day + '/' + event);
                     
                     files.forEach(function(file){
                         if (file == '.' || file == '..')
-                            return false;
+                            return;
                         
                         var ext = path.extname(file).toLowerCase().replace('.', '');
                         
-                        if (ext == 'jpg' || ext == 'jpeg' || ext == 'gif' || ext == 'png') {
+                        if (ext == 'jpg' || ext == 'jpeg' || ext == 'gif' || ext == 'png')
                             event_res.preview_photo = file;
-                            return true;
-                        }
-                        
-                        return true;
                     });
                     
                     result.push(event_res);
-                    
-                    return true;
                 });
-                
-                return true;
             });
-            
-            return true;
         });
-        
-        return true;
     });
     
     return result;
-}
+};
